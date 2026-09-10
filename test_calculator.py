8aef0b1c3479ca04f54b67d65453a94c3f753678
aneesh-dg <aneesh.jantikar@datagrokr.co>
2026-09-10 10:00:45 +0000

    inital commit

---
+++ test_calculator.py
@@ -0,0 +1,18 @@
+import unittest
+from calci import add, subtract, multiply
+
+
+class TestCalculator(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(5, 3), 8)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(5, 3), 15)
+
+
+if __name__ == "__main__":
+    unittest.main()
